23314b502992b289366a55c00d61ea427d9d89c7
Thomas Nøkling <thomas.nokling@volte.no>
2024-04-17 12:02:38 +0200

    Log key path

--- elhub_sdk/client.py
+++ elhub_sdk/client.py
@@ -78,9 +78,11 @@ class APIClient:
 
         """
         history = HistoryPlugin()
-        if(key_file is None or cert_file is None):
+        if (key_file is None or key_file == "") or (cert_file is None or cert_file == ""):
             key_file = os.getenv('ELHUB_KEY_FILE')
             cert_file = os.getenv('ELHUB_CERT_FILE')
+            logger.info(f"Key file: {key_file}")
+            logger.info(f"Cert file: {cert_file}")
         client_settings = Settings(strict=False)
         binary_signature = None
         if secure:
