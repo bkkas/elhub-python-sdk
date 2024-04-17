2ccdabe374e16b01ae9f328d51756e628460547e
Thomas Nøkling <thomas.nokling@volte.no>
2024-04-17 09:34:33 +0200

    Log envelope

--- elhub_sdk/client.py
+++ elhub_sdk/client.py
@@ -1,6 +1,7 @@
 """
 ElHub API client
 """
+import logging
 import os
 from datetime import datetime, timedelta, timezone
 from enum import Enum
@@ -11,6 +12,7 @@ from zeep.plugins import HistoryPlugin
 from zeep.wsse import utils
 from zeep.wsse.signature import BinarySignature
 
+logger = logging.getLogger()
 
 class ElHubService(str, Enum):
     """
@@ -131,6 +133,8 @@ class BinarySignatureTimestamp(BinarySignature):
         timestamp.append(utils.WSU('Expires', expired.replace(microsecond=0).isoformat() + 'Z'))
 
         security.append(timestamp)
+        logger.info(f"BinarySignatureTimestamp envelope: {envelope}")
+        logger.info(f"BinarySignatureTimestamp headers: {headers}")
         super().apply(envelope, headers)
         return envelope, headers
 
